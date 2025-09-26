67acdfe15ad46fdc0d6d16a53d801bbf6a615a36
Siddharthh18 <siddharth@sanskartechnolab.com>
2025-09-26 13:20:50 +0530

    gl report override changes

--- smartclaims/override/general_ledger.py
+++ smartclaims/override/general_ledger.py
@@ -160,6 +160,11 @@ def custom_add_tax_gl_entries(self, gl_entries):
                     )
                 )        
 
+PaymentEntry.add_tax_gl_entries = custom_add_tax_gl_entries
+
+from erpnext.accounts.report.general_ledger import general_ledger as gl_report
+from erpnext.accounts.report.general_ledger.general_ledger import get_gl_entries,get_conditions
+from erpnext.accounts.report.utils import convert_to_presentation_currency, get_currency,convert
 
 def get_party_name_map():
 	party_map = {}
@@ -174,10 +179,57 @@ def get_party_name_map():
 	party_map["Employee"] = {e.name: e.employee_name for e in employees}
 	return party_map
 
-PaymentEntry.add_tax_gl_entries = custom_add_tax_gl_entries
 
-from erpnext.accounts.report.general_ledger import general_ledger as gl_report
-from erpnext.accounts.report.general_ledger.general_ledger import get_gl_entries,convert_to_presentation_currency,get_conditions,get_currency
+def convert_to_presentation_currency(gl_entries, currency_info, filters=None):
+	"""
+	Take a list of GL Entries and change the 'debit' and 'credit' values to currencies
+	in `currency_info`.
+	:param gl_entries:
+	:param currency_info:
+	:return:
+	"""
+	converted_gl_list = []
+	presentation_currency = currency_info["presentation_currency"]
+	company_currency = currency_info["company_currency"]
+
+	account_currencies = list(set(entry["account_currency"] for entry in gl_entries))
+	exchange_gain_or_loss = False
+
+	if filters and isinstance(filters.get("account"), list):
+		account_filter = filters.get("account")
+		gain_loss_account = frappe.db.get_value("Company", filters.company, "exchange_gain_loss_account")
+
+		exchange_gain_or_loss = len(account_filter) == 1 and account_filter[0] == gain_loss_account
+
+	for entry in gl_entries:
+		debit = flt(entry["debit"])
+		credit = flt(entry["credit"])
+		debit_in_account_currency = flt(entry["debit_in_account_currency"])
+		credit_in_account_currency = flt(entry["credit_in_account_currency"])
+		account_currency = entry["account_currency"]
+
+		if (
+			len(account_currencies) == 1
+			and account_currency == presentation_currency
+			and not exchange_gain_or_loss
+		) and not (filters and filters.get("show_amount_in_company_currency")):
+			entry["debit"] = debit_in_account_currency
+			entry["credit"] = credit_in_account_currency
+		else:
+			date = currency_info["report_date"]
+			converted_debit_value = convert(debit, presentation_currency, company_currency, date)
+			converted_credit_value = convert(credit, presentation_currency, company_currency, date)
+
+			if entry.get("debit"):
+				entry["debit"] = converted_debit_value
+
+			if entry.get("credit"):
+				entry["credit"] = converted_credit_value
+
+		converted_gl_list.append(entry)
+
+	return converted_gl_list
+
 
 def custom_get_gl_entries(filters, accounting_dimensions):
     currency_map = get_currency(filters)
