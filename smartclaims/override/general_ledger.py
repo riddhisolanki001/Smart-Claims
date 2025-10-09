bf7b878c02a6a7fb2abe34e67edc0a8ce835379e
Siddharthh18 <siddharth@sanskartechnolab.com>
2025-10-09 11:31:18 +0530

    closing opening total value changes in gl report

--- smartclaims/override/general_ledger.py
+++ smartclaims/override/general_ledger.py
@@ -2,7 +2,7 @@ import frappe
 from erpnext.accounts import general_ledger as gl_module
 from erpnext.accounts.general_ledger import distribute_gl_based_on_cost_center_allocation, toggle_debit_credit_if_negative,merge_similar_entries
 from erpnext.accounts.utils import (
-	get_account_currency
+    get_account_currency
 )
 from frappe.utils import flt
 from collections import OrderedDict
@@ -170,68 +170,68 @@ from erpnext.accounts.report.general_ledger.general_ledger import get_gl_entries
 from erpnext.accounts.report.utils import convert_to_presentation_currency, get_currency,convert
 
 def get_party_name_map():
-	party_map = {}
+    party_map = {}
 
-	customers = frappe.get_all("Customer", fields=["name", "customer_name"])
-	party_map["Customer"] = {c.name: c.customer_name for c in customers}
+    customers = frappe.get_all("Customer", fields=["name", "customer_name"])
+    party_map["Customer"] = {c.name: c.customer_name for c in customers}
 
-	suppliers = frappe.get_all("Supplier", fields=["name", "supplier_name"])
-	party_map["Supplier"] = {s.name: s.supplier_name for s in suppliers}
+    suppliers = frappe.get_all("Supplier", fields=["name", "supplier_name"])
+    party_map["Supplier"] = {s.name: s.supplier_name for s in suppliers}
 
-	employees = frappe.get_all("Employee", fields=["name", "employee_name"])
-	party_map["Employee"] = {e.name: e.employee_name for e in employees}
-	return party_map
+    employees = frappe.get_all("Employee", fields=["name", "employee_name"])
+    party_map["Employee"] = {e.name: e.employee_name for e in employees}
+    return party_map
 
 
 def convert_to_presentation_currency(gl_entries, currency_info, filters=None):
-	"""
-	Take a list of GL Entries and change the 'debit' and 'credit' values to currencies
-	in `currency_info`.
-	:param gl_entries:
-	:param currency_info:
-	:return:
-	"""
-	converted_gl_list = []
-	presentation_currency = currency_info["presentation_currency"]
-	company_currency = currency_info["company_currency"]
+    """
+    Take a list of GL Entries and change the 'debit' and 'credit' values to currencies
+    in `currency_info`.
+    :param gl_entries:
+    :param currency_info:
+    :return:
+    """
+    converted_gl_list = []
+    presentation_currency = currency_info["presentation_currency"]
+    company_currency = currency_info["company_currency"]
 
-	account_currencies = list(set(entry["account_currency"] for entry in gl_entries))
-	exchange_gain_or_loss = False
+    account_currencies = list(set(entry["account_currency"] for entry in gl_entries))
+    exchange_gain_or_loss = False
 
-	if filters and isinstance(filters.get("account"), list):
-		account_filter = filters.get("account")
-		gain_loss_account = frappe.db.get_value("Company", filters.company, "exchange_gain_loss_account")
+    if filters and isinstance(filters.get("account"), list):
+        account_filter = filters.get("account")
+        gain_loss_account = frappe.db.get_value("Company", filters.company, "exchange_gain_loss_account")
 
-		exchange_gain_or_loss = len(account_filter) == 1 and account_filter[0] == gain_loss_account
+        exchange_gain_or_loss = len(account_filter) == 1 and account_filter[0] == gain_loss_account
 
-	for entry in gl_entries:
-		debit = flt(entry["debit"])
-		credit = flt(entry["credit"])
-		debit_in_account_currency = flt(entry["debit_in_account_currency"])
-		credit_in_account_currency = flt(entry["credit_in_account_currency"])
-		account_currency = entry["account_currency"]
+    for entry in gl_entries:
+        debit = flt(entry["debit"])
+        credit = flt(entry["credit"])
+        debit_in_account_currency = flt(entry["debit_in_account_currency"])
+        credit_in_account_currency = flt(entry["credit_in_account_currency"])
+        account_currency = entry["account_currency"]
 
-		if (
-			len(account_currencies) == 1
-			and account_currency == presentation_currency
-			and not exchange_gain_or_loss
-		) and not (filters and filters.get("show_amount_in_company_currency")):
-			entry["debit"] = debit_in_account_currency
-			entry["credit"] = credit_in_account_currency
-		else:
-			date = currency_info["report_date"]
-			converted_debit_value = convert(debit, presentation_currency, company_currency, date)
-			converted_credit_value = convert(credit, presentation_currency, company_currency, date)
+        if (
+            len(account_currencies) == 1
+            and account_currency == presentation_currency
+            and not exchange_gain_or_loss
+        ) and not (filters and filters.get("show_amount_in_company_currency")):
+            entry["debit"] = debit_in_account_currency
+            entry["credit"] = credit_in_account_currency
+        else:
+            date = currency_info["report_date"]
+            converted_debit_value = convert(debit, presentation_currency, company_currency, date)
+            converted_credit_value = convert(credit, presentation_currency, company_currency, date)
 
-			if entry.get("debit"):
-				entry["debit"] = converted_debit_value
+            if entry.get("debit"):
+                entry["debit"] = converted_debit_value
 
-			if entry.get("credit"):
-				entry["credit"] = converted_credit_value
+            if entry.get("credit"):
+                entry["credit"] = converted_credit_value
 
-		converted_gl_list.append(entry)
+        converted_gl_list.append(entry)
 
-	return converted_gl_list
+    return converted_gl_list
 
 
 def custom_get_gl_entries(filters, accounting_dimensions):
@@ -401,142 +401,145 @@ def custom_get_gl_entries(filters, accounting_dimensions):
 
 
 def group_by_field(group_by):
-	if group_by == "Categorize by Party":
-		return "party"
-	elif group_by in ["Categorize by Voucher (Consolidated)", "Categorize by Account"]:
-		return "account"
-	else:
-		return "voucher_no"
+    if group_by == "Categorize by Party":
+        return "party"
+    elif group_by in ["Categorize by Voucher (Consolidated)", "Categorize by Account"]:
+        return "account"
+    else:
+        return "voucher_no"
 
 def get_account_type_map(company):
-	account_type_map = frappe._dict(
-		frappe.get_all("Account", fields=["name", "account_type"], filters={"company": company}, as_list=1)
-	)
+    account_type_map = frappe._dict(
+        frappe.get_all("Account", fields=["name", "account_type"], filters={"company": company}, as_list=1)
+    )
 
-	return account_type_map
+    return account_type_map
 
 def custom_get_accountwise_gle(filters, accounting_dimensions, gl_entries, gle_map, totals):
-	entries = []
-	consolidated_gle = OrderedDict()
-	group_by = group_by_field(filters.get("categorize_by"))
-	group_by_voucher_consolidated = filters.get("categorize_by") == "Categorize by Voucher (Consolidated)"
+    entries = []
+    consolidated_gle = OrderedDict()
+    group_by = group_by_field(filters.get("categorize_by"))
+    group_by_voucher_consolidated = filters.get("categorize_by") == "Categorize by Voucher (Consolidated)"
 
-	if filters.get("show_net_values_in_party_account"):
-		account_type_map = get_account_type_map(filters.get("company"))
+    if filters.get("show_net_values_in_party_account"):
+        account_type_map = get_account_type_map(filters.get("company"))
 
-	immutable_ledger = frappe.db.get_single_value("Accounts Settings", "enable_immutable_ledger")
+    immutable_ledger = frappe.db.get_single_value("Accounts Settings", "enable_immutable_ledger")
 
-	def update_value_in_dict(data, key, gle):
-		data[key].debit += gle.debit
-		data[key].credit += gle.credit
+    def update_value_in_dict(data, key, gle):
+        data[key].debit += gle.debit
+        data[key].credit += gle.credit
 
-		data[key].debit_in_account_currency += gle.debit_in_account_currency
-		data[key].credit_in_account_currency += gle.credit_in_account_currency
+        data[key].debit_in_account_currency += gle.debit_in_account_currency
+        data[key].credit_in_account_currency += gle.credit_in_account_currency
 
-		if filters.get("add_values_in_transaction_currency") and key not in ["opening", "closing", "total"]:
-			data[key].debit_in_transaction_currency += gle.debit_in_transaction_currency
-			data[key].credit_in_transaction_currency += gle.credit_in_transaction_currency
+        if filters.get("add_values_in_transaction_currency") and key not in ["opening", "closing", "total"]:
+            data[key].debit_in_transaction_currency += gle.debit_in_transaction_currency
+            data[key].credit_in_transaction_currency += gle.credit_in_transaction_currency
 
-		if filters.get("show_net_values_in_party_account") and account_type_map.get(data[key].account) in (
-			"Receivable",
-			"Payable",
-		):
-			net_value = data[key].debit - data[key].credit
-			net_value_in_account_currency = (
-				data[key].debit_in_account_currency - data[key].credit_in_account_currency
-			)
+        if filters.get("show_net_values_in_party_account") and account_type_map.get(data[key].account) in (
+            "Receivable",
+            "Payable",
+        ):
+            net_value = data[key].debit - data[key].credit
+            net_value_in_account_currency = (
+                data[key].debit_in_account_currency - data[key].credit_in_account_currency
+            )
 
-			if net_value < 0:
-				dr_or_cr = "credit"
-				rev_dr_or_cr = "debit"
-			else:
-				dr_or_cr = "debit"
-				rev_dr_or_cr = "credit"
+            if net_value < 0:
+                dr_or_cr = "credit"
+                rev_dr_or_cr = "debit"
+            else:
+                dr_or_cr = "debit"
+                rev_dr_or_cr = "credit"
 
-			data[key][dr_or_cr] = abs(net_value)
-			data[key][dr_or_cr + "_in_account_currency"] = abs(net_value_in_account_currency)
-			data[key][rev_dr_or_cr] = 0
-			data[key][rev_dr_or_cr + "_in_account_currency"] = 0
+            data[key][dr_or_cr] = abs(net_value)
+            data[key][dr_or_cr + "_in_account_currency"] = abs(net_value_in_account_currency)
+            data[key][rev_dr_or_cr] = 0
+            data[key][rev_dr_or_cr + "_in_account_currency"] = 0
 
-		if data[key].against_voucher and gle.against_voucher:
-			data[key].against_voucher += ", " + gle.against_voucher
+        if data[key].against_voucher and gle.against_voucher:
+            data[key].against_voucher += ", " + gle.against_voucher
 
-	from_date, to_date = getdate(filters.from_date), getdate(filters.to_date)
-	show_opening_entries = filters.get("show_opening_entries")
+    from_date, to_date = getdate(filters.from_date), getdate(filters.to_date)
+    show_opening_entries = filters.get("show_opening_entries")
  
-	apply_tax_withholding = False
+    apply_tax_withholding = False
 
-	# Get all unique voucher_nos from the GL entries
-	if filters.get("categorize_by") == "Categorize by Voucher (Consolidated)":
-		voucher_nos = list({gle.get("voucher_no") for gle in gl_entries if gle.get("voucher_type") == "Payment Entry"})
+    # Get all unique voucher_nos from the GL entries
+    if filters.get("categorize_by") == "Categorize by Voucher (Consolidated)":
+        voucher_nos = list({gle.get("voucher_no") for gle in gl_entries if gle.get("voucher_type") == "Payment Entry"})
 
-		for voucher_no in voucher_nos:
-			# Safely fetch Payment Entry
-			if frappe.db.exists("Payment Entry", voucher_no):
-				apply_flag = frappe.db.get_value("Payment Entry", voucher_no, "apply_tax_withholding_amount")
-				if apply_flag:
-					apply_tax_withholding = True
-					break
+        for voucher_no in voucher_nos:
+            # Safely fetch Payment Entry
+            if frappe.db.exists("Payment Entry", voucher_no):
+                apply_flag = frappe.db.get_value("Payment Entry", voucher_no, "apply_tax_withholding_amount")
+                if apply_flag:
+                    apply_tax_withholding = True
+                    break
 
-		if apply_tax_withholding:
-			print("Tax Withholding Detected — Returning GL entries as-is")
-			entries = gl_entries[:]  
-			return totals, entries
+        if apply_tax_withholding:
+            for gle in gl_entries:
+                update_value_in_dict(totals, "total", gle)
+                update_value_in_dict(totals, "closing", gle)
+
+            entries = gl_entries[:]  # copy all as-is
+            return totals, entries
 
 
-	# --- otherwise continue your normal logic ---
-	for gle in gl_entries:
-		group_by_value = gle.get(group_by)
-		gle.voucher_type = gle.voucher_type
+    # --- otherwise continue your normal logic ---
+    for gle in gl_entries:
+        group_by_value = gle.get(group_by)
+        gle.voucher_type = gle.voucher_type
 
-		if gle.posting_date < from_date or (cstr(gle.is_opening) == "Yes" and not show_opening_entries):
-			if not group_by_voucher_consolidated:
-				update_value_in_dict(gle_map[group_by_value].totals, "opening", gle)
-				update_value_in_dict(gle_map[group_by_value].totals, "closing", gle)
+        if gle.posting_date < from_date or (cstr(gle.is_opening) == "Yes" and not show_opening_entries):
+            if not group_by_voucher_consolidated:
+                update_value_in_dict(gle_map[group_by_value].totals, "opening", gle)
+                update_value_in_dict(gle_map[group_by_value].totals, "closing", gle)
 
-			update_value_in_dict(totals, "opening", gle)
-			update_value_in_dict(totals, "closing", gle)
+            update_value_in_dict(totals, "opening", gle)
+            update_value_in_dict(totals, "closing", gle)
 
-		elif gle.posting_date <= to_date or (cstr(gle.is_opening) == "Yes" and show_opening_entries):
-			if not group_by_voucher_consolidated:
-				update_value_in_dict(gle_map[group_by_value].totals, "total", gle)
-				update_value_in_dict(gle_map[group_by_value].totals, "closing", gle)
-				update_value_in_dict(totals, "total", gle)
-				update_value_in_dict(totals, "closing", gle)
+        elif gle.posting_date <= to_date or (cstr(gle.is_opening) == "Yes" and show_opening_entries):
+            if not group_by_voucher_consolidated:
+                update_value_in_dict(gle_map[group_by_value].totals, "total", gle)
+                update_value_in_dict(gle_map[group_by_value].totals, "closing", gle)
+                update_value_in_dict(totals, "total", gle)
+                update_value_in_dict(totals, "closing", gle)
 
-				gle_map[group_by_value].entries.append(gle)
+                gle_map[group_by_value].entries.append(gle)
 
-			elif group_by_voucher_consolidated:
-				keylist = [
-					gle.get("posting_date"),
-					gle.get("voucher_type"),
-					gle.get("voucher_no"),
-					gle.get("account"),
-					gle.get("party_type"),
-					gle.get("party"),
-				]
+            elif group_by_voucher_consolidated:
+                keylist = [
+                    gle.get("posting_date"),
+                    gle.get("voucher_type"),
+                    gle.get("voucher_no"),
+                    gle.get("account"),
+                    gle.get("party_type"),
+                    gle.get("party"),
+                ]
 
-				if immutable_ledger:
-					keylist.append(gle.get("creation"))
+                if immutable_ledger:
+                    keylist.append(gle.get("creation"))
 
-				if filters.get("include_dimensions"):
-					for dim in accounting_dimensions:
-						keylist.append(gle.get(dim))
-					keylist.append(gle.get("cost_center"))
-					keylist.append(gle.get("project"))
+                if filters.get("include_dimensions"):
+                    for dim in accounting_dimensions:
+                        keylist.append(gle.get(dim))
+                    keylist.append(gle.get("cost_center"))
+                    keylist.append(gle.get("project"))
 
-				key = tuple(keylist)
-				if key not in consolidated_gle:
-					consolidated_gle.setdefault(key, gle)
-				else:
-					update_value_in_dict(consolidated_gle, key, gle)
+                key = tuple(keylist)
+                if key not in consolidated_gle:
+                    consolidated_gle.setdefault(key, gle)
+                else:
+                    update_value_in_dict(consolidated_gle, key, gle)
 
-	for value in consolidated_gle.values():
-		update_value_in_dict(totals, "total", value)
-		update_value_in_dict(totals, "closing", value)
-		entries.append(value)
+    for value in consolidated_gle.values():
+        update_value_in_dict(totals, "total", value)
+        update_value_in_dict(totals, "closing", value)
+        entries.append(value)
 
-	return totals, entries
+    return totals, entries
 
 
 gl_report.get_gl_entries = custom_get_gl_entries
